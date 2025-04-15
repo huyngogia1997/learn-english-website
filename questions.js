bdf187e395950a031d00518c0c6eebbd14071009
Huy Ngo <huy.ngo@tyme.com>
2025-04-15 21:00:54 +0700

    initial commit

---
+++ questions.js
@@ -0,0 +1,392 @@
+[
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/chair--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "chair",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃeə(r)/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/cheese--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "cheese",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃiːz/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/check--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "check",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃek/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/choose--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "choose",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃuːz/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/chalk--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "chalk",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃɔːk/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/chicken--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "chicken",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /ˈtʃɪkən/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/cheap--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "cheap",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃiːp/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/chin--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "chin",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃɪn/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/chapter--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "chapter",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /ˈtʃæptə(r)/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/cheer--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "cheer",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃɪə(r)/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/child--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "child",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃaɪld/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/chess--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "chess",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃes/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/chat--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "chat",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃæt/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/chase--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "chase",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃeɪs/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/church--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "church",
+      "correctAnswer": "1",
+      "pronunciation": "/tʃ/ - /tʃɜːtʃ/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/jungle--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "jungle",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /ˈdʒʌŋɡl/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/joke--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "joke",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒəʊk/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/judge--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "judge",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒʌdʒ/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/juice--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "juice",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒuːs/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/jam--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "jam",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒæm/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/jeans--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "jeans",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒiːnz/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/job--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "job",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒɒb/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/jump--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "jump",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒʌmp/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/join--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "join",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒɔɪn/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/jog--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "jog",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒɒɡ/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/joy--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "joy",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒɔɪ/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/general--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "general",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /ˈdʒenrəl/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/giraffe--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "giraffe",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /dʒəˈræf/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/giant--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "giant",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /ˈdʒaɪənt/"
+    },
+    {
+      "audio": "https://ssl.gstati3.com/dictionary/static/sounds/oxford/January--_gb_1.mp3",
+      "options": [
+        "1. /tʃ/",
+        "2. /dʒ/",
+        "3. /ʒ/",
+        "4. /d/",
+        "5. /ʃ/"
+      ],
+      "word": "January",
+      "correctAnswer": "2",
+      "pronunciation": "/dʒ/ - /ˈdʒænjuəri/"
+    }
+  ]
